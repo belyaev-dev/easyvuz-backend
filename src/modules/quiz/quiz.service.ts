6f2a35c9481b26c963c13fd678adc2920fc2f48a
Denis Belyaev <battler73@yandex.ru>
2023-07-30 08:03:52 +0300

    feat: return from the db

--- src/modules/quiz/quiz.service.ts
+++ src/modules/quiz/quiz.service.ts
@@ -2,6 +2,7 @@
 import { Injectable } from '@nestjs/common';
 import { OpenAIExecutor } from '@pezzo/integrations';
 import { Pezzo } from '@pezzo/client';
+import { PrismaService } from 'nestjs-prisma';
 
 const { OPENAI_API_KEY, PEZZO_API_KEY, PEZZO_SERVER_URL } = process.env;
 
@@ -17,6 +18,8 @@ export class QuizService {
     apiKey: OPENAI_API_KEY, // <-- Your OpenAI API key
   });
 
+  constructor(readonly prisma: PrismaService) {}
+
   async generateAnswer(answers: any) {
     try {
       const data = await this.openai.run('ProgramTypeSelector', {
@@ -30,12 +33,33 @@ export class QuizService {
         ANSWER_8: answers.responses[7].response,
       });
 
-      console.log(data);
       const parsed = JSON.parse(data.result);
-      console.log(parsed);
-      return parsed;
+      const codes = parsed.map((item) => item.code);
+
+      const programTypesWithProgramsAndUniversities =
+        await this.prisma.programType.findMany({
+          where: {
+            code: {
+              in: codes,
+            },
+          },
+          include: {
+            programs: {
+              include: {
+                university: true,
+              },
+            },
+          },
+        });
+      return programTypesWithProgramsAndUniversities;
     } catch (error) {
       console.error('error', error);
+      // const handler = await this.openai.run('PromptErrorHandler', {
+      //   PROMPT:
+      //     'ProgramTypeSelector that selects the program from the list and returns filtered JSON',
+      //   ANSWER: data ?? {},
+      //   ERROR: error,
+      // });
       let message;
 
       if (error.response?.errors) {
